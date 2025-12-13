d780f1c8730d5642b0524242515051fea411679d
Jaynil Patil <jaynilpatil2007@gmail.com>
2025-12-13 15:20:07 +0530

    About Data types

---
+++ dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") // In node.js it is not defined, but it is define when we use browser
+
+console.log("jaynil");
+
+let name = "Jaynil"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/flase
+// null => standalone value
+// undefine => 
+// symbol => unique
+
+
+// object 
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof isLoggedIn)
+console.log(typeof state)
