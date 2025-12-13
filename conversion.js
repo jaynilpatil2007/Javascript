49dff7ae017d128ecf0e345fa0be672fac3ad049
Jaynil Patil <jaynilpatil2007@gmail.com>
2025-12-13 16:02:41 +0530

    conversion of dataTypes

---
+++ conversion.js
@@ -0,0 +1,31 @@
+let score = false
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+// "33" => 33
+// "33abc" => NaN
+// null => 0
+// undefined => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "jbsil"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false
+// "" => false
+// "jbskauh" => true
+
+let someNumber = 33
+
+let stringSomeNumber = String(someNumber)
+console.log(typeof stringSomeNumber)
+console.log(stringSomeNumber)
